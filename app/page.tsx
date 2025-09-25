33563363fd2fdc3d2f29b8a3424649f0ba47c75e
forumevi <35641620+forumevi@users.noreply.github.com>
2025-09-25 20:58:49 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { Transaction } from '@base-org/minikit-sdk';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Echo</h1>
+      <p>Add a short onchain echo to any cast.</p>
+      <p>Coming soon: 👍 💡 🔥</p>
+      {/* Buraya ileride Transaction bileşeni gelecek */}
+    </div>
+  );
+}
